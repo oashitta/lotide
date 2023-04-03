d577a0d0adae4b25a90dba036f12526c4040756f
Omotunde Shitta <103114829+oashitta@users.noreply.github.com>
2023-04-03 16:42:52 -0400

    assertEqual.js assignment template literals

--- assertEqual.js
+++ assertEqual.js
@@ -2,8 +2,8 @@ const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    // console.log(`❌❌❌ Assertion failed: ${actual} !=== ${expected}`);
-    console.log("❌❌❌ Assertion failed: " + actual + " !=== " + expected);
+    console.log(`❌❌❌ Assertion failed: ${actual} !=== ${expected}`);
+    // console.log("❌❌❌ Assertion failed: " + actual + " !=== " + expected);
   }
 };
 
