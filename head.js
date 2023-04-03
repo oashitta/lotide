2fe21ed8170df9c821077aaf1547d1a4a4268210
Omotunde Shitta <103114829+oashitta@users.noreply.github.com>
2023-04-03 17:10:23 -0400

    head assignment

---
+++ head.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion failed: ${actual} !=== ${expected}`);
+  }
+};
+
+const head = function(array){
+  return array[0];
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), "Hello");
+assertEqual(head([20]), 20);
+assertEqual(head([20]), 15);
+
+
