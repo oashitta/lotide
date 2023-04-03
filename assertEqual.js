a6de3f8fbc9afc95a1520dbdb132428832da7984
Omotunde Shitta <103114829+oashitta@users.noreply.github.com>
2023-04-03 16:28:18 -0400

    assertEqual.js assignment correction

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,9 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`❌❌❌Assertion failed: ${actual} !=== ${expected}`);
+    // console.log(`❌❌❌ Assertion failed: ${actual} !=== ${expected}`);
+    console.log("❌❌❌ Assertion failed: " + actual + " !=== " + expected);
   }
 };
 
