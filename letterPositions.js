e6586375e7e8001d174b5e310fea2ba05e448df7
Omotunde Shitta <103114829+oashitta@users.noreply.github.com>
2023-04-05 18:47:35 -0400

    added letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,54 @@
+
+const eqArrays = function (arr1, arr2) {
+  if(arr1.length !== arr2.length) {
+    return false;
+  }
+  for(let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]){
+      return false;
+    }
+  };
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)){
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`)
+  }else {
+    console.log(`❌❌❌ Assertion failed: ${arr1} !== ${arr2}`);
+  }
+}
+
+// console.log(assertArraysEqual(["hello"], ["hello"]));
+
+
+
+// new function called letterPositions
+// function will return all indicies (zero-based positions) in the string where each xter is found - so it'll print every index at which the letter appears. 
+const letterPositions = function(sentence) {
+  let result = {};
+  // loop through the sentence.
+  for (let i = 0; i < sentence.length; i++){
+        // where letter is occuring for the first time, create a key value pair of key (letter): value (array of index)
+    // console.log(sentence[i]);
+    let letter = sentence[i];
+    if(!result[letter]){
+      result[letter] = [i]
+      // console.log(result)
+    }
+          // where letter is showing for sunsequent times, add index to value/array created above. use push? result[key].push(index)
+    else{
+      result[letter].push(i);
+    }
+  }
+  // delete result[" "]
+  return result;
+};
+
+console.log(letterPositions("hello"));
+console.log(letterPositions("this is a longer sentence"));
+console.log(assertArraysEqual(letterPositions("this is a longer sentence").o, [ 11 ]))
+console.log(assertArraysEqual(letterPositions("this is a longer sentence").i, [ 2, 5 ]))
+console.log(assertArraysEqual(letterPositions("this is a longer sentence").s, [ 3, 6, 17 ]))
+console.log(assertArraysEqual(letterPositions("this is a longer sentence").n, [ 12, 19, 22  ]))
+// console.log(assertArraysEqual(letterPositions("hello").e, [1]));
