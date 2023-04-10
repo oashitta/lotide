0d61c880c1523d33437172d702e3636391cab0ad
Omotunde Shitta <103114829+oashitta@users.noreply.github.com>
2023-04-10 14:13:12 -0400

    installed mocha and chai and used them to rest the head.js

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,32 @@
+const assert = require('chai').assert;
 const head = require('../head');
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual'); //no longer required because we are using the chai assertion libarary.
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), "Hello");
-assertEqual(head([20]), 20);
-assertEqual(head([20]), 15);
+describe ("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+
+  it ("returns 20 for [20]", () => {
+    assert.strictEqual(head([20]), 20);
+  });
+
+  it(`returns hello for ["Hello", "Lighthouse", "Labs"]`, () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+  it ("returns empty array for empty array", () => {
+    assert.strictEqual(head([]), );
+  });
+
+
+});
+
