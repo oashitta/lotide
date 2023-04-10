cfd7d7fab97f8c9dd156f4617234464e09a6a204
Omotunde Shitta <103114829+oashitta@users.noreply.github.com>
2023-04-10 13:12:06 -0400

    added test folder and modules for assertEqual.js and head.js

---
+++ test/assertEqualTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require('../assertEqual');
+
+// Test Code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("coffee", "coffee");
+assertEqual("coffee", "milk");
+assertEqual(88, 88);
+assertEqual(88, 58);
